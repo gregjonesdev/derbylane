f1ac852d731d0eae2ec75a083392bb2dc957d955
Greg <gmjones@gmail.com>
2021-05-03 00:09:03 -0500

    reads json data

--- miner/management/commands/bootstrap.py
+++ miner/management/commands/bootstrap.py
@@ -1,6 +1,12 @@
+import json
+
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write("Unterminated line")
+        self.stdout.write("Starting Bootstrap script..")
+        data = open('./static/json/data.json').read()
+        jsonData = json.loads(data)
+        venues = jsonData["venues"]
+        print(len(venues))
