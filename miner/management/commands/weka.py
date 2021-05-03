8f70769928cc01775e3bc7e823cf77f749ceacf8
Greg <gmjones@gmail.com>
2021-05-03 10:23:23 -0500

    loads weka script

--- miner/management/commands/weka.py
+++ miner/management/commands/weka.py
@@ -13,5 +13,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Starting Weka script..")
         jvm.start(packages=True)
-        data = conv.load_any_file(os.environ.get("MOOC_DATA") + os.sep+"anneal.arff")
+        data = conv.load_any_file("./rawdat/arff/airline.arff")
+        print(data)
         jvm.stop()
