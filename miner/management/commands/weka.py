bc35725f838a3d531401266a5d7856f1bd983860
Greg <gmjones@gmail.com>
2021-05-03 10:54:59 -0500

    weka evaluation working

--- miner/management/commands/weka.py
+++ miner/management/commands/weka.py
@@ -8,11 +8,20 @@ import weka.core.converters as conv
 from weka.classifiers import Evaluation, Classifier
 from weka.core.classes import Random
 
+confidence_vector = "0.3"
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write("Starting Weka script..")
+        self.stdout.write("Starting Weka script..\n")
         jvm.start(packages=True)
-        data = conv.load_any_file("./rawdat/arff/airline.arff")
-        print(data)
+        data = conv.load_any_file("./rawdat/arff/weather.numeric.arff")
+        data.class_is_last()
+        cls = Classifier(
+            classname="weka.classifiers.trees.J48",
+            options=["-C", "0.3"])
+        evl = Evaluation(data)
+        evl.crossvalidate_model(cls, data, 10, Random(1))
+        self.stdout.write(evl.summary("=== J48 on weather (numeric): Stats ===", False))
+        self.stdout.write(evl.matrix("=== J48 on weather (numeric): Confusion Matrix"))
         jvm.stop()
