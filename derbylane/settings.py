d1b1412d179b786c9a031a5e07c5b343e87e9f67
Greg <gmjones@gmail.com>
2021-05-06 22:01:15 -0500

    try default settings

---
+++ derbylane/settings.py
@@ -0,0 +1,21 @@
+from settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['www.cashdog.life', 'cashdog.life', '143.198.234.238',]
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'derbylane',
+        'USER': 'derbylaneuser',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
--- derbylane/settings/base.py
+++ derbylane/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from django.conf import settings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
