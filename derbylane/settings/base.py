c967d76e0350b4abe5b5f1b8571eb6840aff3e2a
Greg <gmjones@gmail.com>
2021-05-06 23:33:11 -0500

    ROOT_URLCONF

--- derbylane/settings/base.py
+++ derbylane/settings/base.py
@@ -17,7 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-settings(ROOT_URLCONF) = "cashdog.urls"
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
