700e3dc98f766f740adf71129dfe5526b9afc59a
Greg <gmjones@gmail.com>
2021-05-07 23:53:17 -0500

    add pww to packages

--- derbylane/settings.py
+++ derbylane/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'miner',
     'rawdat',
     'derbylane',
+    'pww',
 ]
 
 MIDDLEWARE = [
--- derbylane/settingsconf/base.py
+++ derbylane/settingsconf/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'miner',
     'rawdat',
     'derbylane',
+    'pww',
 ]
 
 MIDDLEWARE = [
