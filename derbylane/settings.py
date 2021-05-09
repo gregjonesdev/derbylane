8d665e6fd190c291c43b3abffd4e4aa527f0ed1d
Greg <gmjones@gmail.com>
2021-05-09 00:48:42 -0500

    fix typo

--- derbylane/settings.py
+++ derbylane/settings.py
@@ -1,5 +1,5 @@
 import os
-from .settingsconf.bse import *
+from .settingsconf.base import *
 ROOT_URLCONF = "derbylane.urls"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
