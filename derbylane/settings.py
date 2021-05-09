7fcb18a7289b2adb9643c7a32cca20848269ce55
Greg <gmjones@gmail.com>
2021-05-09 00:52:10 -0500

    inherit installed apps

--- derbylane/settings.py
+++ derbylane/settings.py
@@ -21,20 +21,20 @@ STATICFILES_DIRS = (
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'codes',
-    'users',
-    'miner',
-    'rawdat',
-    'derbylane',
-    'pww',
-]
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'codes',
+#     'users',
+#     'miner',
+#     'rawdat',
+#     'derbylane',
+#     'pww',
+# ]
 
 LOGIN_URL = 'two_factor:login'
 
