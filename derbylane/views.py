bd8f231ff85a010f1ccf4bb05dc051536fabc927
Greg <gmjones@gmail.com>
2021-05-18 18:16:03 -0500

    roll back shit

--- derbylane/views.py
+++ derbylane/views.py
@@ -10,7 +10,7 @@ from rawdat.models import (
 )
 
 
-class Welcome(OTPRequiredMixin, View):
+class Welcome(View):
 
     template_name = "welcome.html"
     context = {}
@@ -18,19 +18,19 @@ class Welcome(OTPRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, self.context)
 
-class FrontPage(OTPRequiredMixin, View):
+class FrontPage(View):
 
     template_name = 'frontpage.html'
     context = {}
 
     def get(self, request, *args, **kwargs):
         today = datetime.now()
-        venues = []
-        for chart in Chart.objects.filter(program__date = today):
-            venue = chart.program.venue
-            if not venue in venues:
-                venues.append(venue)
-        self.context["venues"] = venues
-        self.context["today"] = today.strftime("%A, %B %-d")
+        # venues = []
+        # for chart in Chart.objects.filter(program__date = today):
+        #     venue = chart.program.venue
+        #     if not venue in venues:
+        #         venues.append(venue)
+        # self.context["venues"] = venues
+        # self.context["today"] = today.strftime("%A, %B %-d")
 
         return render(request, self.template_name, self.context)
