2e9c270e49ecaf6b39013d3b9231cac12f4a0472
Greg <gmjones@gmail.com>
2021-05-18 22:05:52 -0500

    Revert "fix up frontpage"
    This reverts commit 12e0ac0cd3c7a4e34335e967e83d70e2531cdc51.

--- derbylane/urls.py
+++ derbylane/urls.py
@@ -8,7 +8,6 @@ from two_factor.gateways.twilio.urls import urlpatterns as tf_twilio_urls
 from derbylane.views import (
     Welcome,
     FrontPage,
-    load_charts,
 )
 
 def buildURL(object_name):
@@ -30,5 +29,4 @@ urlpatterns = [
         name='frontpage'),
     path('', include(tf_twilio_urls)),
     path('accounts/', include('django.contrib.auth.urls')),
-    url(r'load_charts/$', load_charts, name='load_charts'),
 ]
