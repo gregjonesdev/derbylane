210a6c2017d313f8b176939ac9b54b0095540862
Greg <gmjones@gmail.com>
2021-05-21 23:12:38 -0500

    opens file

--- pww/management/commands/make_predictions.py
+++ pww/management/commands/make_predictions.py
@@ -1,4 +1,5 @@
 import datetime
+import csv
 
 from django.core.management.base import BaseCommand
 from rawdat.models import Race, Venue
@@ -24,18 +25,23 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        for venue in Venue.objects.filter(is_active=True):
-            for distance in venue_distances[venue.code]:
-                for grade_name in valued_grades:
-                    races = Race.objects.filter(
-                        chart__program__date=datetime.date.today(),
-                        grade__name=grade_name,
-                        distance=distance
-                    )
-                    print("{} Grade {} {} yds".format(venue.name, grade_name, distance))
-                    print("----------------------------------")
-                    relevant_metrics = self.get_relevant_metrics(venue, grade_name, distance)
-                    completed_metrics = relevant_metrics.filter(final__isnull=False)
-                    scheduled_metrics = relevant_metrics.filter(final__isnull=True)
-                    print(len(relevant_metrics))
-                    print("\n")
+        filename = '/media/greg/85b2a17a-2087-4963-8d42-f1720b3be0d1/greg/work/derbylane/dl_env/derbylane/scheduled.csv'
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(row)
+        # for venue in Venue.objects.filter(is_active=True):
+        #     for distance in venue_distances[venue.code]:
+        #         for grade_name in valued_grades:
+        #             races = Race.objects.filter(
+        #                 chart__program__date=datetime.date.today(),
+        #                 grade__name=grade_name,
+        #                 distance=distance
+        #             )
+        #             print("{} Grade {} {} yds".format(venue.name, grade_name, distance))
+        #             print("----------------------------------")
+        #             relevant_metrics = self.get_relevant_metrics(venue, grade_name, distance)
+        #             completed_metrics = relevant_metrics.filter(final__isnull=False)
+        #             scheduled_metrics = relevant_metrics.filter(final__isnull=True)
+        #             print(len(relevant_metrics))
+        #             print("\n")
