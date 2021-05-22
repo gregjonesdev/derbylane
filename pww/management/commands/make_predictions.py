378c2b3e267300845f73500141b00d03af1772b9
Greg <gmjones@gmail.com>
2021-05-22 01:13:26 -0500

    arff loader:

--- pww/management/commands/make_predictions.py
+++ pww/management/commands/make_predictions.py
@@ -23,21 +23,29 @@ class Command(BaseCommand):
         )
         return metrics
 
+    def analyze_participants(race):
+        for paricipant in race.participant_set.all():
+            try:
+                metric = Metric.objects.get(partipant=participant)
+                pass
+            except:
+                print("Metric not found for participant: {}".format(partipant.uuid))
+
 
     def handle(self, *args, **options):
-        filename = '/media/greg/85b2a17a-2087-4963-8d42-f1720b3be0d1/greg/work/derbylane/dl_env/derbylane/scheduled.csv'
-        with open(filename, 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                print(row)
-        # for venue in Venue.objects.filter(is_active=True):
-        #     for distance in venue_distances[venue.code]:
-        #         for grade_name in valued_grades:
-        #             races = Race.objects.filter(
-        #                 chart__program__date=datetime.date.today(),
-        #                 grade__name=grade_name,
-        #                 distance=distance
-        #             )
+        # filename = '/media/greg/85b2a17a-2087-4963-8d42-f1720b3be0d1/greg/work/derbylane/dl_env/derbylane/scheduled.csv'
+        # with open(filename, 'r') as f:
+        #     reader = csv.reader(f)
+        #     for row in reader:
+        #         print(row)
+        for venue in Venue.objects.filter(is_active=True):
+            for distance in venue_distances[venue.code]:
+                for grade_name in valued_grades:
+                    for race in Race.objects.filter(
+                        chart__program__date=datetime.date.today(),
+                        grade__name=grade_name,
+                        distance=distance):
+                        analyze_participants(race)
         #             print("{} Grade {} {} yds".format(venue.name, grade_name, distance))
         #             print("----------------------------------")
         #             relevant_metrics = self.get_relevant_metrics(venue, grade_name, distance)
--- pww/management/commands/wekawrap.py
+++ pww/management/commands/wekawrap.py
@@ -9,13 +9,19 @@ from weka.classifiers import Evaluation, Classifier
 from weka.core.classes import Random
 
 confidence_vector = "0.3"
-
+data_dir = "./rawdat/arff/"
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+
+
+
         self.stdout.write("Starting Weka script..\n")
-        jvm.start(packages=True)
-        data = conv.load_any_file("./rawdat/arff/weather.numeric.arff")
+        jvm.start(packages=True, system_info=True)
+        # data = conv.load_any_file("./rawdat/arff/weather.numeric.arff")
+        loader = conv.Loader(classname="weka.core.converters.ArffLoader")
+        data=loader.load_file(data_dir + "weather.numeric.arff")
+
         data.class_is_last()
         cls = Classifier(
             classname="weka.classifiers.trees.J48",
