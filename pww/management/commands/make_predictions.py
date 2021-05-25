8ced56b68ef6bbc409b3cb2114e60971f477c08f
Greg <gmjones@gmail.com>
2021-05-25 00:42:22 -0500

    clean up

--- pww/management/commands/make_predictions.py
+++ pww/management/commands/make_predictions.py
@@ -1,11 +1,13 @@
-import datetime
 import csv
-from django.core.exceptions import ObjectDoesNotExist
-# import os.path
+import datetime
+
 from pathlib import Path
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
-from rawdat.models import Race, Venue
+
 from pww.models import Prediction, Metric
+from rawdat.models import Race, Venue
 
 
 from miner.utilities.constants import (
@@ -19,43 +21,20 @@ from miner.utilities.constants import (
 class Command(BaseCommand):
 
 
-    def analyze_participants(self, race):
-        for participant in race.participant_set.all():
-            try:
-                metric = Metric.objects.get(participant=participant)
-            except:
-                print("Metric not found for participant: {}".format(participant.uuid))
-
-
     def create_csv(self, filename, metrics):
-        print("create csv()")
-        print(filename)
-        # nested_dir = date.replace("_", "/")
-        #
         csv_directory = "csv"
         Path(csv_directory).mkdir(
                 parents=True,
                 exist_ok=True)
-        # raise SystemExit(0)
 
         with open("{}/{}".format(csv_directory, filename), 'w', newline='') as write_obj:
             csv_writer = csv.writer(write_obj)
             csv_writer.writerow(csv_columns)
             for metric in metrics:
-                print(metric.build_csv_metric())
-            #     while len(metric) < len(coded_columns):
-            #         metric.append(0)
-                # print(metric)
                 csv_writer.writerow(metric.build_csv_metric())
-        # print("done!")
 
 
     def handle(self, *args, **options):
-        # filename = '/media/greg/85b2a17a-2087-4963-8d42-f1720b3be0d1/greg/work/derbylane/dl_env/derbylane/scheduled.csv'
-        # with open(filename, 'r') as f:
-        #     reader = csv.reader(f)
-        #     for row in reader:
-        #         print(row)
         today = datetime.date.today()
         for venue in Venue.objects.filter(is_active=True):
             venue_metrics = Metric.objects.filter(
@@ -68,20 +47,8 @@ class Command(BaseCommand):
                     graded_metrics = distance_metrics.filter(
                         participant__race__grade__name=grade_name,
                     )
-                    print("{} Grade {} {} yds".format(venue.name, grade_name, distance))
-                    # print(len(graded_metrics))
                     completed_metrics = graded_metrics.filter(final__isnull=False)
                     scheduled_metrics = graded_metrics.filter(final__isnull=True)
-                    print(len(completed_metrics))
-                    print(len(scheduled_metrics))
                     race_key = "{}_{}_{}".format(venue.code, distance, grade_name)
                     if len(scheduled_metrics) > 0:
                         self.create_csv("{}_scheduled.csv".format(race_key), scheduled_metrics)
-
-
-
-
-# get a list of participants in races for a specific venue, grade, and distance
-# get a list of metrics for each participant
-# get the relevant completed metrics
-# send the two lists to pww for prediction(s)
