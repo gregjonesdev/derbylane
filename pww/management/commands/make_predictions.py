5f111578bff419dcbe10b7c35e2c8a66745c9154
Greg <gmjones@gmail.com>
2021-05-25 02:21:21 -0500

    clean up

--- pww/management/commands/make_predictions.py
+++ pww/management/commands/make_predictions.py
@@ -21,7 +21,7 @@ from miner.utilities.constants import (
 class Command(BaseCommand):
 
 
-    def create_csv(self, filename, metrics):
+    def create_arff(self, filename, metrics):
         arff_file = open(filename, "w")
         arff_file.write("@relation Metric\n")
 
@@ -36,34 +36,17 @@ class Command(BaseCommand):
                 # csv_writer.writerow(["@attribute {} numeric".format(each)])
                 arff_file.write("@attribute {} numeric\n".format(each))
 
-        # with open(filename, 'w', newline='') as write_obj:
-        #     csv_writer = csv.writer(write_obj)
-        #
-        #     csv_writer.writerow(["@relation Metrics"])
-        #     # csv_writer.writerow(["\n"])
-        #
-        #     for each in csv_columns:
-        #         if each == "PID":
-        #             csv_writer.writerow(["@attribute PID string"])
-        #         elif each == "Se":
-        #             csv_writer.writerow(["@attribute Se {M, F}"])
-        #         else:
-        #             csv_writer.writerow(["@attribute {} numeric".format(each)])
-        #
-        #     # csv_writer.writerow(["\n"])
-        #
-        #
-        #     csv_writer.writerow(["@data"])
-        #
         arff_file.write("@data\n")
 
         for metric in metrics:
             arff_file.writelines(metric.build_csv_metric())
 
+        return filename
+
 
     def handle(self, *args, **options):
-        csv_directory = "csv"
-        Path(csv_directory).mkdir(
+        arff_directory = "arff"
+        Path(arff_directory).mkdir(
                 parents=True,
                 exist_ok=True)
         today = datetime.date.today()
@@ -82,13 +65,13 @@ class Command(BaseCommand):
                     scheduled_metrics = graded_metrics.filter(final__isnull=True)
                     race_key = "{}_{}_{}".format(venue.code, distance, grade_name)
                     if len(scheduled_metrics) > 0:
-                        scheduled_filename = "csv/{}_scheduled.arff".format(race_key)
-                        scheduled_csv = self.create_csv(
-                            scheduled_filename,
-                            scheduled_metrics)
-                        results_filename = "csv/{}_results.arff".format(race_key)
+                        scheduled_filename = "arff/{}_scheduled.arff".format(race_key)
+                        results_filename = "arff/{}_results.arff".format(race_key)
 
-                        results_csv = self.create_csv(
-                            results_filename,
-                            completed_metrics)
-                        make_predictions(scheduled_filename, results_filename)
+                        make_predictions(
+                            self.create_arff(
+                                scheduled_filename,
+                                scheduled_metrics),
+                            self.create_arff(
+                                results_filename,
+                                completed_metrics))
