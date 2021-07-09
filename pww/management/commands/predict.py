2561071ebe825197e1a6cfbb0016d1c4b0ea478f
Greg <gmjones@gmail.com>
2021-07-08 21:29:48 -0500

    rename predict

--- pww/management/commands/predict.py
+++ pww/management/commands/predict.py
@@ -1,43 +1,42 @@
-import csv
+# import csv
+import sys
+import os
+import fnmatch
+from pathlib import Path
+import weka.core.jvm as jvm
 import datetime
 
-from pathlib import Path
-
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 
-from pww.models import Prediction, Metric
-from rawdat.models import Race, Venue
-from pww.utilities.weka import predict_all, evaluate_predictions
+from pww.models import Metric
+from rawdat.models import Venue
+from pww.utilities.weka import predict_all
 
 from miner.utilities.constants import (
-    focused_grades,
+    csv_columns,
     focused_distances,
-    csv_columns)
+    focused_grades)
 
 
 class Command(BaseCommand):
 
-
-    def create_arff(self, filename, metrics, is_nominal):
+    def create_arff(self, filename, metrics, start_date):
         arff_file = open(filename, "w")
         arff_file.write("@relation Metric\n")
 
-        arff_file = self.write_headers(arff_file, is_nominal)
+        arff_file = self.write_headers(arff_file)
 
         for metric in metrics:
-            csv_metric = metric.build_csv_metric()
+            csv_metric = metric.build_csv_metric(start_date)
             if csv_metric:
                 arff_file.writelines(csv_metric)
 
         return filename
 
 
-    def write_headers(self, arff_file, is_nominal):
+    def write_headers(self, arff_file):
         for each in csv_columns:
-            if is_nominal and each == "Fi":
-                arff_file.write("@attribute {} nominal\n".format(each))
-            elif each == "PID":
+            if each == "PID":
                 arff_file.write("@attribute PID string\n")
             elif each == "Se":
                 arff_file.write("@attribute Se {M, F}\n")
@@ -48,39 +47,59 @@ class Command(BaseCommand):
         return arff_file
 
 
+    def get_metrics(self, venue_code, distance, grade_name):
+        return Metric.objects.filter(
+            participant__race__chart__program__venue__code=venue_code,
+            participant__race__distance=distance,
+            participant__race__grade__name=grade_name,
+            participant__race__chart__program__date__gte="2019-01-01",
+            final__isnull=False)
+
+    def get_race_keys_to_test(self, models):
+        race_keys_to_test = {}
+        for model in models:
+            if "AA" in model.upper():
+                race_key = model[:9]
+            else:
+                race_key = model[:8]
+            if not race_key in race_keys_to_test.keys():
+                race_keys_to_test[race_key] = []
+            race_keys_to_test[race_key].append(model)
+        return race_keys_to_test
+
 
     def handle(self, *args, **options):
         today = datetime.date.today()
-        scheduled_data = {}
+        scheduled_start = today
+        scheduled_start = "2019-01-01"
+        start_datetime = datetime.datetime.strptime(scheduled_start, "%Y-%m-%d")
+        start_date = start_datetime.date()
+        arff_list = []
         for venue in Venue.objects.filter(is_focused=True):
-            print(venue)
+            print("Building metrics for {}".format(venue))
             venue_code = venue.code
             venue_metrics = Metric.objects.filter(
                 participant__race__chart__program__venue=venue)
             for distance in focused_distances[venue_code]:
-                print("Distance: {}".format(distance))
+                # print("Distance: {}".format(distance))
                 distance_metrics = venue_metrics.filter(
                     participant__race__distance=distance,
                 )
                 for grade_name in focused_grades[venue_code]:
-                    print("Grade: {}".format(grade_name))
+                    # print("Grade: {}".format(grade_name))
                     graded_metrics = distance_metrics.filter(
                         participant__race__grade__name=grade_name,
                     )
-                    scheduled_metrics = graded_metrics.filter(
-                        participant__race__chart__program__date__gte=today)
-
-                    # print("Metrics Found: {}".format(len(scheduled_metrics)))
-                    if len(scheduled_metrics) > 0:
-                        race_key = "{}_{}_{}".format(venue.code, distance, grade_name)
-                    #     race_key = "SL_583_C"
-                        scheduled_filename = "arff/{}_scheduled.arff".format(race_key)
-                        scheduled_data[race_key] = self.create_arff(
-                            scheduled_filename,
-                            scheduled_metrics,
-                            False)
-        for each in scheduled_data:
-            print(scheduled_data[each])
-            fake_model = "WD_548_C_J48_C0_75.model"
-            evaluate_predictions(fake_model, scheduled_data[each] )
-        # predict_all(scheduled_data)
+                    if len(graded_metrics) > 0:
+                        # print(len(graded_metrics))
+                        scheduled_metrics = graded_metrics.filter(
+                        participant__race__chart__program__date__gte=scheduled_start)
+                        if len(scheduled_metrics) > 0:
+                            training_metrics = graded_metrics.filter(
+                                participant__race__chart__program__date__lt=scheduled_start)
+                            # print(len(training_metrics))
+                            race_key = "{}_{}_{}".format(venue_code, distance, grade_name)
+                            arff_list.append(self.create_arff(
+                                "arff/{}.arff".format(race_key),
+                                graded_metrics, start_date))
+        predict_all(arff_list)
