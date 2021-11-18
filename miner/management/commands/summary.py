df823218bf332dbd1b946ac5017d58f280d21b1d
Greg <gmjones@gmail.com>
2021-11-18 16:33:03 -0600

    script

--- miner/management/commands/summary.py
+++ miner/management/commands/summary.py
@@ -14,17 +14,71 @@ from rawdat.models import Participant
 
 class Command(BaseCommand):
 
+    def get_race_key_from_race(self, race):
+        return "{}_{}_{}".format(
+            race.chart.program.venue.code,
+            race.distance,
+            race.grade.name)
+
+
+    def get_win_return(self, participant):
+        try:
+            return float(participant.straight_wager.win)
+        except:
+            return 0
+
+    def get_place_return(self, participant):
+        try:
+            return float(participant.straight_wager.place)
+        except:
+            return 0
+
+    def get_show_return(self, participant):
+        try:
+            return float(participant.straight_wager.show)
+        except:
+            return 0
+
+    def get_average(self, list):
+        if len(list) > 0:
+            round(sum(list)/len(list), 2)
+        return "---"
+    def build_table(self, winnings):
+        print("\nReturns By Race Type:\n")
+        table_string = "{}\t\t{}\t\t{}\t\t{}"
+        print(table_string.format("\t", "Win", "Place", "Show"))
+        for grade in winnings.keys():
+            grade_winnings = winnings[grade]
+            self.write_table_row(grade, grade_winnings, table_string)
+
+    def write_table_row(self, grade, grade_winnings, table_string):
+        print(table_string.format(
+            grade,
+            self.get_average(grade_winnings["W"]),
+            self.get_average(grade_winnings["P"]),
+            self.get_average(grade_winnings["S"])))
+
     def handle(self, *args, **options):
 
         today = datetime.date.today()
         yesterday = (today - datetime.timedelta(days=1))
         last_week = yesterday = (today - datetime.timedelta(days=7))
-        print(today)
+        winnings = {}
         for participant in Participant.objects.filter(
-            race__chart__program__date__gte=last_week):
+            race__chart__program__date=yesterday):
             recommended_bets = participant.get_recommended_bet()
             if recommended_bets:
-                for bet in recommended_bets:
-                    print("W" in bet)
-                    print("P" in bet)
-                    print("S" in bet)
+                race_key = self.get_race_key_from_race(participant.race)
+                if not race_key in winnings.keys():
+                    winnings[race_key] = {"W": [], "P": [], "S":[]}
+                if "W" in recommended_bets:
+                    winnings[race_key]["W"].append(
+                        self.get_win_return(participant))
+                if "P" in recommended_bets:
+                    winnings[race_key]["P"].append(
+                        self.get_place_return(participant))
+                if "S" in recommended_bets:
+                    winnings[race_key]["P"].append(
+                        self.get_place_return(participant))
+
+        self.build_table(winnings)
