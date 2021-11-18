d33f75e903de80e66d68806872ea2cd4f1caed7f
Greg <gmjones@gmail.com>
2021-11-18 09:43:32 -0600

    fix key error

--- pww/management/commands/predict.py
+++ pww/management/commands/predict.py
@@ -86,6 +86,7 @@ class Command(BaseCommand):
         training_metrics,
         prediction_metrics,
         race_key):
+        string_c = None
         try:
             string_c = c_options[classifier_name][race_key]
         except KeyError:
