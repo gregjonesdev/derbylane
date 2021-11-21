ef31522528ff6236859ba170f0fccb1051b84fce
Greg <gmjones@gmail.com>
2021-11-21 03:27:23 -0600

    working with dates

--- derbylane/static/js/frontpage.js
+++ derbylane/static/js/frontpage.js
@@ -5,36 +5,36 @@ const json_data = JSON.parse(text_content)
 
 window.onload = (event) => {
   dropdowns = document.getElementsByClassName("dropdown-item")
-  //
-  // function load_races (e)  {
-  //   console.log("load races")
-  //   const currentTarget = e.currentTarget
-  //   const label = currentTarget.textContent
-  //   const chart_id = e.currentTarget.getAttribute("data-chart")
-  //   load_charts(chart_id, label)
-  // 
-  //
-  // }
 
-  // function load_charts(chart_id, label) {
-  //   console.log("load charts")
-  //       $.ajax({
-  //         url: json_data["bets_url"],
-  //         dataType: "html",
-  //         data: {
-  //           "chart_id": chart_id,
-  //         },
-  //         success: function(data) {
-  //           console.log("loaded charts")
-  //           console.log(data)
-  //           $("#bets").html(data)
-  //           const bets_container =   document.getElementById("bets")
-  //           bets_container.style.maxHeight = "75vh";
-  //           bets_container.style.overflowY = "scroll";
-  //           document.getElementById("chart-label").textContent = label
-  //         }
-  //       })
-  // }
+  function load_races (e)  {
+    console.log("load races")
+    const currentTarget = e.currentTarget
+    const label = currentTarget.textContent
+    const chart_id = e.currentTarget.getAttribute("data-chart")
+    load_charts(chart_id, label)
+
+
+  }
+
+  function load_charts(chart_id, label) {
+    console.log("load charts")
+        $.ajax({
+          url: json_data["bets_url"],
+          dataType: "html",
+          data: {
+            "chart_id": chart_id,
+          },
+          success: function(data) {
+            console.log("loaded charts")
+            console.log(data)
+            $("#bets").html(data)
+            const bets_container =   document.getElementById("bets")
+            bets_container.style.maxHeight = "75vh";
+            bets_container.style.overflowY = "scroll";
+            document.getElementById("chart-label").textContent = label
+          }
+        })
+  }
 
   for (let i=0; i<dropdowns.length; i++) {
     dropdowns[i].addEventListener("click", load_races)
