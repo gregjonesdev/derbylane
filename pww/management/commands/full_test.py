afef9185383f227391ea795d35e4c39433c1befa
Greg <gmjones@gmail.com>
2021-11-24 16:23:40 -0600

    try 2018

--- pww/management/commands/full_test.py
+++ pww/management/commands/full_test.py
@@ -148,7 +148,7 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        venue_code = "SL"
+        venue_code = "TS"
         grade_name = sys.argv[5]
         distance = focused_distances[venue_code][0]
         today = datetime.datetime.now()
