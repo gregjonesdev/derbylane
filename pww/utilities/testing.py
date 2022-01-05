59116577557ad62e39dee578d093f218764a9bdb
Greg <gmjones@gmail.com>
2022-01-05 14:00:05 -0600

    j48 confidence

--- pww/utilities/testing.py
+++ pww/utilities/testing.py
@@ -44,7 +44,8 @@ def get_daily_results(
     training_metrics = all_metrics.filter(
         participant__race__chart__program__date__lt=target_date)
     testing_metrics = all_metrics.filter(
-        participant__race__chart__program__date=target_date)
+        participant__race__chart__program__date__gte=target_date,
+        participant__race__chart__program__date__lt="2022-01-04",)
     is_nominal = False
     training_arff = get_training_arff(
         race_key,
