512998aa8d1f9a449234e76bcfadb7e89f0f6a63
Greg <gmjones@gmail.com>
2022-01-06 00:02:08 -0600

    separate by venue and grade

--- pww/management/commands/single_model.py
+++ pww/management/commands/single_model.py
@@ -15,8 +15,9 @@ from pww.utilities.testing import evaluate_model_cutoffs
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        # parser.add_argument('--grade', type=str)
         parser.add_argument('--prediction', type=str)
+        parser.add_argument('--venue', type=str)
+        parser.add_argument('--grade', type=str)
 
     def handle(self, *args, **options):
         c_factor = 0.4
@@ -35,6 +36,8 @@ class Command(BaseCommand):
             training_metrics,
             is_nominal)
         target_prediction = sys.argv[3]
+        venue_code = sys.argv[5]
+        grade = sys.argv[7]
         jvm.start(
             packages=True,
             max_heap_size="5028m"
@@ -52,23 +55,19 @@ class Command(BaseCommand):
         print("Training Metrics: {}\n".format(
             len(training_metrics)))
 
-        for venue_code in venue_codes:
-            print("Venue: {}".format(venue_code))
-            for grade in focused_grades[venue_code]:
-                print("Grade: {}".format(grade))
-                testing_metrics = Metric.objects.filter(
-                    participant__race__chart__program__venue__code=venue_code,
-                    participant__race__grade__name=grade,
-                    participant__race__chart__program__date__range=(
-                        "2021-12-04",
-                        "2022-01-04"))
-                testing_arff = get_testing_arff(
-                    "{}_{}".format(venue_code, grade),
-                    testing_metrics,
-                    is_nominal)
-                print("Testing Metrics: {}".format(len(testing_metrics)))
-                evaluate_model_cutoffs(
-                    model,
-                    target_prediction,
-                    testing_arff)
+        testing_metrics = Metric.objects.filter(
+            participant__race__chart__program__venue__code=venue_code,
+            participant__race__grade__name=grade,
+            participant__race__chart__program__date__range=(
+                "2021-12-04",
+                "2022-01-04"))
+        testing_arff = get_testing_arff(
+            "{}_{}".format(venue_code, grade),
+            testing_metrics,
+            is_nominal)
+        print("Testing Metrics: {}".format(len(testing_metrics)))
+        evaluate_model_cutoffs(
+            model,
+            target_prediction,
+            testing_arff)
         jvm.stop()
