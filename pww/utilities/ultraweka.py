9b32ab293462553112dc766d89d0b9a3f2256568
Greg <gmjones@gmail.com>
2022-01-11 12:31:49 -0600

    print len predictions

--- pww/utilities/ultraweka.py
+++ pww/utilities/ultraweka.py
@@ -267,6 +267,7 @@ def get_prediction_confidence(testing_arff, model, target_prediction, confidence
             dist = model.distribution_for_instance(inst)
             index = int(prediction) # SMO only 0-7
             confidence = dist[index]
+            print(dist)
             if int(prediction) == int(target_prediction):
                 if confidence >= confidence_cutoff:
                     prediction_confidence[uuid] = (prediction, confidence)
