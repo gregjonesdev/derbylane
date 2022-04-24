2c75c0811a7f285db85dbf3d02f98189a108f071
Greg Jones <gmjones@gmail.com>
2022-04-24 14:54:27 -0500

    favicon

--- derbylane/staticfiles/js/frontpage.js
+++ derbylane/staticfiles/js/frontpage.js
@@ -1,44 +1,10 @@
-const text_content = document.getElementById("json-chart-data").textContent
+cacbb9ad4f6c4b3f569d84517bdd28011446e272const text_content = document.getElementById("json-chart-data").textContent
 const json_data = JSON.parse(text_content)
-const modal = document.getElementById("bet-modal")
 
-modal.addEventListener("focus", function (e) {
-  const button = $(event.relatedTarget) // Button that triggered the modal
-  if (button.hasClass("bet-button")) {
-    const participant_bets = document.getElementById("participant_bets")
-    const dog = button.data('dog')
-    const bets = button.data('bets')
-    const post = button.data('post')
-    const venue = button.data('venue')
-    const chart = button.data('chart')
-    const number = button.data('number')
-    const participant = button.data('participant')
-    const this_modal = $(this)
-    this_modal.find('.modal-subtitle').text(
-      post + " | " +
-      dog + " "
-    )
-    this_modal.find('.modal-title').text(
-      venue + " " +
-      chart + " Race " +
-      number
-    )
-    this_modal.find('#participant_uuid').text(
-      participant
-   )
-    // $('select option[value=' + bets).prop('selected',true);
-  }
-
-})
 
 
 window.onload = (event) => {
   dropdowns = document.getElementsByClassName("dropdown-item")
-
-  // function initial_load () {
-  //     console.log(dropdowns[0].textContent)
-  //     console.log(dropdowns[0].getAttribute("data-chart"))
-  // }
 
   function load_races (e)  {
     console.log("load races")
@@ -70,8 +36,6 @@ window.onload = (event) => {
         })
   }
 
-  // initial_load()
-
   for (let i=0; i<dropdowns.length; i++) {
     dropdowns[i].addEventListener("click", load_races)
   }
@@ -80,6 +44,7 @@ window.onload = (event) => {
   const chart_select = document.getElementById("chart-select")
 
   load_bets = () => {
+    console.log("load bets")
     if (chart_select) {
       chart_id = chart_select.value
       $.ajax({
@@ -90,6 +55,8 @@ window.onload = (event) => {
         },
         success: function(data) {
           $("#bets").html(data)
+          console.log("success")
+          console.log(data)
           const bets_container =   document.getElementById("bets")
           bets_container.style.maxHeight = "75vh";
           bets_container.style.overflowY = "scroll";
