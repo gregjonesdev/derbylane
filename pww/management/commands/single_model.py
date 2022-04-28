abbedf97f787fc8f5668aafcdf1b38d643c4d794
Greg Jones <gmjones@gmail.com>
2022-04-28 01:41:45 -0500

    force WD

--- pww/management/commands/single_model.py
+++ pww/management/commands/single_model.py
@@ -30,7 +30,7 @@ class Command(BaseCommand):
         training_metrics = Metric.objects.filter(
             participant__race__grade__name=grade,
             # participant__race__distance=550,
-            participant__race__chart__program__venue__code=venue_code,
+            participant__race__chart__program__venue__code='WD',
             participant__race__chart__program__date__range=(
                 "2020-01-01",
                 "2021-12-31"))
