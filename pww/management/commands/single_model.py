7124a9da79b072dce3963e6a9f62498411ea0c27
Greg Jones <gmjones@gmail.com>
2022-04-28 15:38:17 -0500

    c = 0.5

--- pww/management/commands/single_model.py
+++ pww/management/commands/single_model.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
         parser.add_argument('--prediction', type=str)
 
     def handle(self, *args, **options):
-        c_factor = 0.75
+        c_factor = 0.5
         classifier_name = "j48"
         race_key = "universal"
         venue_code = sys.argv[3]
