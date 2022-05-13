791fc37a1b9e1424ab6c33f81d0aa50fe794bd88
Greg Jones <gmjones@gmail.com>
2022-05-13 00:24:52 -0500

    test for smo_reg

--- pww/utilities/arff.py
+++ pww/utilities/arff.py
@@ -5,7 +5,8 @@ def write_headers(arff_file, is_nominal):
     for each in csv_columns:
         if is_nominal and each == "Fi":
             arff_file.write("@attribute {} nominal\n".format(each))
-        # elif each == "PID":
+        elif each == "PID":
+            pass
         #     arff_file.write("@attribute PID string\n")
         # elif each == "Se":
         #     arff_file.write("@attribute Se {M, F}\n")
