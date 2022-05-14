5c58d08da8c104d51ad8e9e93b0b9b8e3745a0d6
Greg Jones <gmjones@gmail.com>
2022-05-14 03:04:02 -0500

    smoreg test

--- pww/utilities/classifiers.py
+++ pww/utilities/classifiers.py
@@ -15,7 +15,7 @@ classifiers = {
     },
     "j48": {
         "path": "weka.classifiers.trees.J48",
-        "is_nominal": True,
+        "is_nominal": False,
         "options": [],
     },
     "randomforest": {
