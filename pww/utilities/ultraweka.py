79e64abb564d90ee7cb65015ae250fb5786b6ac7
Greg Jones <gmjones@gmail.com>
2022-05-14 16:49:52 -0500

    smoreg test

--- pww/utilities/ultraweka.py
+++ pww/utilities/ultraweka.py
@@ -187,7 +187,7 @@ def evaluate_nominal(classifier, filtered_data, uuid_line_index):
 
     print("{}\t\t{}\t\t{}\t\t{}\t\t{}".format("Range", "Freq", "Win", "Place", "Show"))
     for each in interval_object.keys():
-        string_row = "{} - {}:\t{} ({}%)\t\t{}\t{}\t{}"
+        string_row = "{} - {}\t{} ({}%)\t\t{}\t{}\t{}"
         percent = round((100*len(interval_object[each])/count), 2)
         if len(interval_object[each]) > 99:
             string_row = "{} - {}:\t{} ({}%)\t{}\t{}\t{}"
