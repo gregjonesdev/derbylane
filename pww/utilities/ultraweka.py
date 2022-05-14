68a1fb54150f8e258e811af0bfc64cdaaa96ef8f
Greg Jones <gmjones@gmail.com>
2022-05-14 06:21:28 -0500

    smoreg test

--- pww/utilities/ultraweka.py
+++ pww/utilities/ultraweka.py
@@ -117,7 +117,7 @@ def evaluate_nominal(classifier, filtered_data, uuid_line_index):
             round(float(each), 2),
             round(float(each) + interval, 2),
             len(interval_object[each]),
-            int(100*len(interval_object[each])/count))
+            int(100*len(interval_object[each])/count)))
     print("Min: {}".format(min))
     print("Max: {}".format(max))
 
