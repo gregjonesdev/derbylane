207338067073746db66d11a37355d9b25b89990f
Greg Jones <gmjones@gmail.com>
2022-05-15 17:18:16 -0500

    generates model name

--- miner/management/commands/build_models.py
+++ miner/management/commands/build_models.py
@@ -1,8 +1,15 @@
 from django.core.management.base import BaseCommand
 
-from pww.utilities.constants import model_data
+from pww.utilities.classifiers import model_data
 
 class Command(BaseCommand):
+
+    def get_model_name(self, venue_code, grade_name, start_date):
+        return "{}_{}_{}".format(
+            venue_code,
+            grade_name,
+            start_date.replace("-", "_")
+        )
 
     def handle(self, *args, **options):
         for model in model_data:
@@ -10,3 +17,5 @@ class Command(BaseCommand):
             print(model["end_date"])
             print(model["venue_code"])
             print(model["grade_name"])
+            print(self.get_model_name(model["venue_code"], model["grade_name"], model["start_date"]))
+            print("\n")
