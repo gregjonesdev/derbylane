62f75c9bf3b17f6866191d0e9cd5952265496bf5
Greg Jones <gmjones@gmail.com>
2022-05-28 03:29:08 -0500

    change tri, qui, and exa

--- miner/management/commands/fixes.py
+++ miner/management/commands/fixes.py
@@ -20,11 +20,88 @@ from pww.models import Participant_Prediction
 
 class Command(BaseCommand):
 
+    def change_quinellas(self, quinellas):
+        for quinella in quinellas:
+            race = quinella.left.race
+            payout = quinella.payout
+            try:
+                sizzle = Sizzle_Quinella.objects.get(
+                    race = race,
+                    left = quinella.left,
+                    right = quinella.right
+                )
+            except ObjectDoesNotExist:
+                try:
+                    sizzle = Sizzle_Quinella.objects.get(
+                        race = race,
+                        left = quinella.right,
+                        right = quinella.left
+                    )
+                except ObjectDoesNotExist:
+                    new_sizzle = Sizzle_Quinella(
+                        race = race,
+                        left = quinella.right,
+                        right = quinella.left,
+                        payout = payout
+                    )
+                    new_sizzle.set_fields_to_base()
+                    new_sizzle.save()
+        print(Sizzle_Quinella.objects.all().count())
+
+    def change_exactas(self, exactas):
+        for exacta in exactas:
+            race = exacta.win.race
+            payout = exacta.payout
+            try:
+                sizzle = Sizzle_Exacta.objects.get(
+                    race = race,
+                    win = exacta.win,
+                    place = exacta.place
+                )
+            except ObjectDoesNotExist:
+                new_sizzle = Sizzle_Exacta(
+                    race = race,
+                    win = exacta.win,
+                    place = exacta.place,
+                    payout = payout
+                )
+                new_sizzle.set_fields_to_base()
+                new_sizzle.save()
+        print(Sizzle_Exacta.objects.all().count())
+
+    def change_trifectas(self, trifectas):
+        for trifecta in trifectas:
+            race = trifecta.win.race
+            payout = trifecta.payout
+            try:
+                sizzle = Sizzle_Trifecta.objects.get(
+                    race = race,
+                    win = trifecta.win,
+                    place = trifecta.place,
+                    show = trifecta.show
+                )
+            except ObjectDoesNotExist:
+                new_sizzle = Sizzle_Trifecta(
+                    race = race,
+                    win = trifecta.win,
+                    place = trifecta.place,
+                    show = trifecta.show,
+                    payout = payout
+                )
+                new_sizzle.set_fields_to_base()
+                new_sizzle.save()
+        print(Sizzle_Trifecta.objects.all().count())
+
+
+
     def handle(self, *args, **options):
         print(Quiniela.objects.all().count())
+        self.change_quinellas(Quiniela.objects.all())
         print(Exacta.objects.all().count())
+        self.change_exactas(Exacta.objects.all())
         print(OldTrifecta.objects.all().count())
-        print(Superfecta.objects.all().count())
-        print(Bet.objects.all().count())
-        print(Straight_Wager.objects.all().count())
-        print(Participant_Prediction.objects.all().count())
+        self.change_trifectas(OldTrifecta.objects.all())
+        # print(Superfecta.objects.all().count())
+        # print(Bet.objects.all().count())
+        # print(Straight_Wager.objects.all().count())
+        # print(Participant_Prediction.objects.all().count())
