c4801c852133f7223c0f043ce7b6d8237a4b1754
Greg Jones <gmjones@gmail.com>
2022-05-27 20:56:42 -0500

    continue work parsing exotic bets

--- miner/utilities/new_scrape.py
+++ miner/utilities/new_scrape.py
@@ -66,13 +66,56 @@ def get_exotic_bet_list(tds):
                 exotic_bet_list.append(each.text)
     return exotic_bet_list
 
+def get_posts_index(split_text):
+    for entry in split_text:
+        if "/" in entry:
+            return split_text.index(entry)
+
+def get_payout(split_text):
+    payout = split_text[-1]
+    return float(
+        payout.replace(
+        "$", "").replace(
+        ",", ""))
+
+def get_exotic_name(split_text, posts_index):
+    type_text = split_text[1:posts_index]
+    if len(type_text) > 1:
+        new_type = ""
+        for each in type_text:
+            new_type += "{} ".format(each)
+        return new_type.strip()
+    else:
+        return type_text[0]
+
+def get_posts_list(split_text, posts_index):
+    posts_list = []
+    posts = split_text[posts_index]
+    for post in posts.split("/"):
+        try:
+            posts_list.append(int(post))
+        except:
+            print("handle exception")
+            # find posts in table: get_finish_order
+
+            print(post_list)
+            print(url)
+            raise SystemExit(0)
+
+
+
+def parse_exotic_bets(split_text):
+    payout = get_payout(split_text)
+    posts_index =  get_posts_index(split_text)
+    exotic_name = get_exotic_name(split_text, posts_index)
+
+    print(payout)
+    print(post_list)
+    print(exotic_name)
 
 def save_exotic_bets(race, tds):
-
-    exotic_bet_list = get_exotic_bet_list(tds)
-    print(race)
-    for exotic_bet in exotic_bet_list:
-        print(exotic_bet)
+    for exotic_bet_text in get_exotic_bet_list(tds):
+        parse_exotic_bets(exotic_bet_text.split())
     raise SystemExit(0)
 
 
