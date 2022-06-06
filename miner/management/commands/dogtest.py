715210a7b1839dad6ed78516f4e93fc7f6dde62e
Greg Jones <gmjones@gmail.com>
2022-06-05 19:16:05 -0500

    scan again

--- miner/management/commands/dogtest.py
+++ miner/management/commands/dogtest.py
@@ -7,7 +7,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Magic number: 24")
         dog = Dog.objects.get(name="SUPER C DEMURE")
-        print(dog.participant_set.filter(
+        parts = dog.participant_set.filter(
+            race__distance=550,
             race__condition__name="F",
             final__isnull=False,
-        ))
+        )
+        for part in parts:
+            print(part.race.chart.program.date)
