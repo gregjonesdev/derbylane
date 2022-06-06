89b69a089d7f5ea1226a3a851f543b5a23fd899c
Greg Jones <gmjones@gmail.com>
2022-06-05 19:09:32 -0500

    scan again

--- miner/management/commands/dogtest.py
+++ miner/management/commands/dogtest.py
@@ -5,6 +5,6 @@ from rawdat.models import Dog
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("Magic number: 28")
+        print("Magic number: 24")
         dog = Dog.objects.get(name="SUPER C DEMURE")
         print(dog.participant_set.all().count())
