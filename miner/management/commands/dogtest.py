4438cd4ed8419d6069b0291399b7a4f72002e030
Greg Jones <gmjones@gmail.com>
2022-06-05 19:13:04 -0500

    scan again

--- miner/management/commands/dogtest.py
+++ miner/management/commands/dogtest.py
@@ -7,4 +7,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Magic number: 24")
         dog = Dog.objects.get(name="SUPER C DEMURE")
-        print(dog.participant_set.all().count())
+        print(dog.participant_set.filter(
+            race__condition__name="F",
+            final__isnull=False,
+        ))
