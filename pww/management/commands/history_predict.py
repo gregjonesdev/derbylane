1ff8bde8b68f7a26ad717fe8d919c54dad017d8c
Greg Jones <gmjones@gmail.com>
2022-06-14 04:35:39 -0500

    history predict

--- pww/management/commands/history_predict.py
+++ pww/management/commands/history_predict.py
@@ -19,13 +19,8 @@ from pww.utilities.weka import (
 
 class Command(BaseCommand):
 
-    def make_straight_predictions(self, race, weka_model, graded_metrics):
-        print("Straight predcition")
-        print(weka_model.training_start)
-        print(weka_model.training_end)
-        print(weka_model.classifier.name)
+    def make_straight_predictions(self, weka_model, graded_metrics):
         classifier = weka_model.classifier
-        print(weka_model)
         make_predictions(
             weka_model,
             get_prediction_arff(graded_metrics),
@@ -34,7 +29,7 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        # start_jvm()
+        start_jvm()
         start_date = datetime.datetime(2015, 1, 1)
         end_date = datetime.datetime(2020, 12, 31)
         for venue in Venue.objects.filter(is_active=True):
@@ -46,29 +41,8 @@ class Command(BaseCommand):
                     venue.code,
                     start_date,
                     end_date)
+                 self.make_straight_predictions(
+                    weka_model,
+                    graded_metrics)
                 print(len(metrics))
-
-        # print("scheduled_metrics")
-        # print(scheduled_metrics.count())
-        # for race in Race.objects.filter(chart__program__date__gte=today):
-        #     print(race.number)
-        #     grade = race.grade
-        #     venue = race.chart.program.venue
-        #     weka_models = WekaModel.objects.filter(
-        #         venue=venue,
-        #         grade=grade
-        #     )
-        #     print("Weka models: {}".format(weka_models.count()))
-        #     if weka_models.count() > 0:
-        #         graded_metrics = scheduled_metrics.filter(
-        #             participant__race__chart__program__venue=venue,
-        #             participant__race__grade=grade)
-        #         print("Graded Mterics: {}".format(len(graded_metrics)))
-        #         for weka_model in weka_models:
-        #             print(weka_model.get_name())
-        #             self.make_straight_predictions(
-        #                 race,
-        #                 weka_model,
-        #                 graded_metrics)
-
-        # stop_jvm()
+        stop_jvm()
